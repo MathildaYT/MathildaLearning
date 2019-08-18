dd53663f3c59d5608f85bc400a321c5ac5d765a8
赵雨婷 <805076814@qq.com>
2019-08-18 15:54:41 +0800

    给两条路径，随机一条走

--- Pac_Man/Assets/Scripts/GhostMove.cs
+++ Pac_Man/Assets/Scripts/GhostMove.cs
@@ -1,38 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using UnityEngine;
+﻿    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using UnityEngine;
 
-public class GhostMove:MonoBehaviour
-{
-    public GameObject wayPointGo;
-    private List<Vector3> points = new List<Vector3>();
-    int index = 0;
-    float speed = 0.03f;
-    void Start()
+    public class GhostMove:MonoBehaviour
     {
-        foreach (Transform item in wayPointGo.transform)
+        public GameObject[] wayPointGos;
+        private List<Vector3> points = new List<Vector3>();
+        int index = 0;
+        float speed = 0.03f;
+        void Start()
         {
-            points.Add(item.position);
-        }
-    }
-    private void FixedUpdate()
-    {
-        if (transform.position!=points[index])
-        {
-            Vector3 temp = Vector3.MoveTowards(transform.position, points[index], speed);
-            this.GetComponent<Rigidbody>().MovePosition(temp);
-        }
-        else
-        {
-            index++;
-            if (index==points.Count)
+            foreach (Transform item in wayPointGos[UnityEngine.Random.Range(0, 2)].transform)
             {
-                index = 0;
+                points.Add(item.position);
             }
+        }
+        private void FixedUpdate()
+        {
+            if (transform.position!=points[index])
+            {
+                Vector3 temp = Vector3.MoveTowards(transform.position, points[index], speed);
+                this.GetComponent<Rigidbody>().MovePosition(temp);
+            }
+            else
+            {
+                index++;
+                if (index==points.Count)
+                {
+                    index = 0;
+                    points.Clear();
+                    foreach (Transform item in wayPointGos[UnityEngine.Random.Range(0, 2)].transform)
+                    {
+                        points.Add(item.position);
+                    }
+                }
+            }
+            }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag=="player")
+        {
+            Destroy(other.gameObject);
         }
     }
 }
+
 
